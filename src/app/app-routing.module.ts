b66c93d93ce2fe3f9453fddbd16a486e82eb9e6f
Carranza12 <franckgta3@gmail.com>
2023-05-28 10:20:08 -0600

    cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', 
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+  }, 
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'prefix'
+  },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
